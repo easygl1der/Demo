2f4ce237525ecfcf62d1351cd5da6a55ae354083
easyglider <easygl1der@outlook.com>
2024-05-02 01:22:15 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家', items:[
+        {text:'首页',link:'/'},
+        {text:'markdown示例',link:'/markdown-examples'}
+      ] },
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer:{
+      copyright:"Copyright@ 2024-present by Yue Yihua"
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,29 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "我的文档项目"
+  text: "A VitePress Site"
+  tagline: 过去从未消失，未来已经存在。
+  
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: http://www.jd.com
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+    - theme: alt
+      text: document
+      link: /mydownload_link
+
+features:
+  - title: Feature A
+    details: 特征一的详细描述
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
