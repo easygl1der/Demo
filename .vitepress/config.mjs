11db3b21467c75681a0b4cdfe1c9ba04994e9c82
easyglider <easygl1der@outlook.com>
2024-05-02 11:46:42 +0800

    注释base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,8 @@ import { set_sidebar } from "./utils/auto_sidebar.mjs";	// 改成自己的路径
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  //base: "/docs-demo/",
+  head: [["link", {rel: "icon", href: "/docs-demo/logo.svg" }]],
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
