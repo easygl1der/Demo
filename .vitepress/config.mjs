00eaa6972d7c979d804e84d386b222f81ffacef0
easygl1der <159306110+easygl1der@users.noreply.github.com>
2024-05-02 15:11:05 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { set_sidebar } from "./utils/auto_sidebar.mjs"; // 改成自己的路径
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/docs-demo/",
+  base: "/Demo/",
   head: [["link", { rel: "icon", href: "/docs-demo/logo.svg" }]],
   title: "我的厉害的项目",
   description: "A VitePress Site",
